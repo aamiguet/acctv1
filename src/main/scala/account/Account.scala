f009d7afab8e6a49f5b2d77089387bc60fc1ca42
Antoine Amiguet <amiguet.antoine@gmail.com>
2014-11-05 22:02:53 +0100

    Done for today (11/5/2014)

--- src/main/scala/account/Account.scala
+++ src/main/scala/account/Account.scala
@@ -4,10 +4,10 @@ import scala.slick.driver.H2Driver.simple._
 import scala.slick.lifted.{ProvenShape, ForeignKeyQuery}
 
 class Account(tag: Tag) 
-  extends Table[(Int, String)](tag, "ACCOUNT"){
+  extends Table[(Option[Int], String)](tag, "ACCOUNT"){
   
   def id: Column[Int] = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def name: Column[String] = column[String]("name")
 
-  def * : ProvenShape[(Int, String)] = (id, name)
+  def * : ProvenShape[(Option[Int], String)] = (id ?, name)
 }
--- src/main/scala/account/HelloAccount.scala
+++ src/main/scala/account/HelloAccount.scala
@@ -3,5 +3,22 @@ package account
 import scala.slick.driver.H2Driver.simple._
 
 object HelloAccount extends App {
+  
+  val account: TableQuery[Account] = TableQuery[Account]
+  
+  val db = Database.forURL("jdbc:h2:mem:hello", driver = "org.h2.Driver")
+  db.withSession { implicit session =>
+    account.ddl create
+    
+    account += (None, "antoine")
+    account += (None, "alfred")
+    
+    val accounts: List[(Option[Int], String)] = account.list
+    
+    accounts foreach {
+      case (Some(id), name) => println(s"Account of $name, $id")
+      case (None, name) => println(s"Account of $name")
+    }
+  }
 
 }
